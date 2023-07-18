9b4d603b311f57487d6db0d213d88deceb3d9169
AhmetSIRIM <ahmet.sirim@outlook.com>
2023-07-18 18:36:21 +0300

    Add splash screen

--- app/src/main/java/com/whale/bendingbuddies/ui/MainActivity.kt
+++ app/src/main/java/com/whale/bendingbuddies/ui/MainActivity.kt
@@ -2,12 +2,14 @@ package com.whale.bendingbuddies.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.whale.bendingbuddies.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
